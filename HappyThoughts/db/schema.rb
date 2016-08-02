20c45a6fb002ee72f1c682984489b65966bb8c82
Kisuk Yu <kisuk.ryu@gmail.com>
2016-08-02 16:20:45 -0400

    set up welcome page

--- HappyThoughts/db/schema.rb
+++ HappyThoughts/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802150530) do
+ActiveRecord::Schema.define(version: 20160802174349) do
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
