0ebc0f94672991771aaf06e6f3d624fe4dfea208
91juhwang <91juhwang@gmail.com>
2016-08-02 16:02:40 -0400

    added navigation, created User/Post model association and profile section

--- HappyThoughts/app/models/post.rb
+++ HappyThoughts/app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+	belongs_to :user
+	validates :body,
+		presence: true,
+		length: { minimum: 20 }
 end
--- HappyThoughts/db/schema.rb
+++ HappyThoughts/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802150530) do
+ActiveRecord::Schema.define(version: 20160802190220) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
