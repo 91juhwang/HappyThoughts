5229b20610fc70d9ff6db4fb50929f84a310b5df
Kisuk Yu <kisuk.ryu@gmail.com>
2016-08-03 11:39:38 -0400

    added paperclip to project

--- HappyThoughts/app/controllers/posts_controller.rb
+++ HappyThoughts/app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
 
 	private
 		def post_params
-			params.require(:post).permit(:body, :user_id)
+			params.require(:post).permit(:body, :user_id, :image)
 		end
 
 end
--- HappyThoughts/db/schema.rb
+++ HappyThoughts/db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802204222) do
+ActiveRecord::Schema.define(version: 20160803151538) do
 
   create_table "posts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
     t.string   "body"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
