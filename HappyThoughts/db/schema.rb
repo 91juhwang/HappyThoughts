4e839877c78c479a0eb7c7cce5297a6eb9d712d0
Imelda Alonso <adlewic@gmail.com>
2016-08-03 15:21:24 -0400

    first style

--- HappyThoughts/db/schema.rb
+++ HappyThoughts/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802204222) do
+ActiveRecord::Schema.define(version: 20160802190220) do
 
   create_table "posts", force: :cascade do |t|
+    t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.string   "body"
   end
 
   create_table "users", force: :cascade do |t|
