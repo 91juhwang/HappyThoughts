0ad360d6f560a47ab8aaaa3308785658ca9544e7
Kisuk Yu <kisuk.ryu@gmail.com>
2016-08-07 21:43:17 -0400

    made some minor changes to some labels and added in username to all post in welcome page

--- HappyThoughts/app/models/post.rb
+++ HappyThoughts/app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_attached_file :image, :styles => { large: "320x320>", thumb: "100x100#"}
+	has_attached_file :image, :styles => { large: "320x320>", thumb: "100x100>"}
 	# , :default_url => "images/:styles/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	
